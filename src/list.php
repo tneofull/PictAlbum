9f917cdd0e2f577cf0b8ed40e7a8332b1cece48e
tneofull <103440576+tneofull@users.noreply.github.com>
2022-06-01 23:37:08 +0900

    一覧ページのモック作成

---
+++ src/list.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>画像一覧</title>
+</head>
+
+<body>
+    <header>
+        <h1>PhotoAlbum</h1>
+    </header>
+
+    <ul>
+        <li>
+            <div>
+                <p>公園の名前:日暮里公園</p>
+            </div>
+            <div>
+                <p>公園の場所:荒川区</p>
+            </div>
+            <div>
+                <p>景観:きれい</p>
+            </div>
+            <div>
+                <p>大きさ:small</p>
+            </div>
+            <div>
+                <p>総合評価:5</p>
+            </div>
+            <div>
+                <p>感想:構造が良い感じ。<br>
+                    広さも適度にある。
+                </p>
+            </div>
+        </li>
+        <li>
+            <div>
+                <p>公園の名前:お花茶屋公園</p>
+            </div>
+            <div>
+                <p>公園の場所:葛飾区</p>
+            </div>
+            <div>
+                <p>景観:きれい</p>
+            </div>
+            <div>
+                <p>大きさ:small</p>
+            </div>
+            <div>
+                <p>総合評価:5</p>
+            </div>
+            <div>
+                <p>感想:構造が良い感じ。<br>
+                    椅子の数が多い。
+                </p>
+            </div>
+        </li>
+    </ul>
+
+    <a href="index.php" style="margin: 0 120px;">検索ページへ</a>
+    <a href="list.php" style="margin: 0 120px;">登録ページへ</a>
+
+</body>
+
+</html>
