b13769a59bb56ea8fe750fce409b89d9b0edba9b
tneofull <103440576+tneofull@users.noreply.github.com>
2022-06-05 12:05:22 +0900

    一覧表示画面用のデータ取得し、HTML反映まで完了

--- src/dbConnection.php
+++ src/dbConnection.php
@@ -4,7 +4,6 @@
 function dbConnection()
 {
     if ($link = mysqli_connect('db', 'book_log', 'pass', 'book_log')) {
-        echo 'データベースに接続しました' . PHP_EOL;
     } else {
         echo 'Error: データベース接続に失敗しました' . PHP_EOL;
         echo 'Debugging Error: ' . mysqli_connect_error() . PHP_EOL;
--- src/list.php
+++ src/list.php
@@ -16,15 +16,13 @@ function selectTable($link)
     FROM parks
 EOT;
 
-    if ($result = mysqli_query($link, $sql)):
-        echo 'データの取得に成功しました' . PHP_EOL;
+    if ($result = mysqli_query($link, $sql)) :
         $parks = [];
-        while ($row = mysqli_fetch_assoc($result)):
+        while ($row = mysqli_fetch_assoc($result)) :
             $parks[] = $row;
         endwhile;
-        // var_dump($parks);
         return $parks;
-    else:
+    else :
         echo 'Error: データの取得に失敗しました' . PHP_EOL;
         echo 'Debugging Error: ' . mysqli_error($link) . PHP_EOL;
     endif;
@@ -32,15 +30,6 @@ EOT;
 
 $link = dbConnection();
 $parks = selectTable($link);
-
-foreach ($parks as $park):
-    echo $park['name'] . PHP_EOL;
-    echo $park['area'] . PHP_EOL;
-    echo $park['view'] . PHP_EOL;
-    echo $park['size'] . PHP_EOL;
-    echo $park['score'] . PHP_EOL;
-    echo $park['comment'] . PHP_EOL;
-endforeach;
 
 ?>
 
@@ -57,53 +46,30 @@ endforeach;
     <header>
         <h1>PhotoAlbum</h1>
     </header>
-
-    <ul>
-        <li>
-            <div>
-                <p>公園の名前:日暮里公園</p>
-            </div>
-            <div>
-                <p>公園の場所:荒川区</p>
-            </div>
-            <div>
-                <p>景観:きれい</p>
-            </div>
-            <div>
-                <p>大きさ:small</p>
-            </div>
-            <div>
-                <p>総合評価:5</p>
-            </div>
-            <div>
-                <p>感想:構造が良い感じ。<br>
-                    広さも適度にある。
-                </p>
-            </div>
-        </li>
-        <li>
-            <div>
-                <p>公園の名前:お花茶屋公園</p>
-            </div>
-            <div>
-                <p>公園の場所:葛飾区</p>
-            </div>
-            <div>
-                <p>景観:きれい</p>
-            </div>
-            <div>
-                <p>大きさ:small</p>
-            </div>
-            <div>
-                <p>総合評価:5</p>
-            </div>
-            <div>
-                <p>感想:構造が良い感じ。<br>
-                    椅子の数が多い。
-                </p>
-            </div>
-        </li>
-    </ul>
+    <?php foreach ($parks as $park) : ?>
+        <ul>
+            <li>
+                <div>
+                    <p>公園の名前: <?php echo $park['name'] . PHP_EOL; ?></p>
+                </div>
+                <div>
+                    <p>公園の場所: <?php echo $park['area'] . PHP_EOL; ?></p>
+                </div>
+                <div>
+                    <p>景観: <?php echo $park['view'] . PHP_EOL; ?></p>
+                </div>
+                <div>
+                    <p>大きさ: <?php echo $park['size'] . PHP_EOL; ?></p>
+                </div>
+                <div>
+                    <p>総合評価: <?php echo $park['score'] . PHP_EOL; ?></p>
+                </div>
+                <div>
+                    <p>感想: <?php echo $park['comment'] . PHP_EOL; ?><br></p>
+                </div>
+            </li>
+        </ul>
+    <?php endforeach; ?>
 
     <a href="index.php" style="margin: 0 120px;">検索ページへ</a>
     <a href="new.php" style="margin: 0 120px;">登録ページへ</a>
