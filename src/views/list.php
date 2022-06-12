c788763aa05236bfc859eba4d4542262d753b17b
tneofull <103440576+tneofull@users.noreply.github.com>
2022-06-12 14:08:56 +0900

    画像をフレックスで横表示のためcssファイルを導入

--- src/views/list.php
+++ src/views/list.php
@@ -2,22 +2,24 @@
         <ul>
             <li>
                 <div>
-                    <p>公園の名前: <?php echo $park['name'] . PHP_EOL; ?></p>
-                </div>
-                <div>
-                    <p>公園の場所: <?php echo $park['area'] . PHP_EOL; ?></p>
-                </div>
-                <div>
-                    <p>景観: <?php echo $park['view'] . PHP_EOL; ?></p>
-                </div>
-                <div>
-                    <p>大きさ: <?php echo $park['size'] . PHP_EOL; ?></p>
-                </div>
-                <div>
-                    <p>総合評価: <?php echo $park['score'] . PHP_EOL; ?></p>
-                </div>
-                <div>
-                    <p>感想: <?php echo $park['comment'] . PHP_EOL; ?><br></p>
+                    <div>
+                        <p>公園の名前: <?php echo $park['name'] . PHP_EOL; ?></p>
+                    </div>
+                    <div>
+                        <p>公園の場所: <?php echo $park['area'] . PHP_EOL; ?></p>
+                    </div>
+                    <div>
+                        <p>景観: <?php echo $park['view'] . PHP_EOL; ?></p>
+                    </div>
+                    <div>
+                        <p>大きさ: <?php echo $park['size'] . PHP_EOL; ?></p>
+                    </div>
+                    <div>
+                        <p>総合評価: <?php echo $park['score'] . PHP_EOL; ?></p>
+                    </div>
+                    <div>
+                        <p>感想: <?php echo $park['comment'] . PHP_EOL; ?><br></p>
+                    </div>
                 </div>
                 <div>
                     <img src="/img/<?php echo $park['pictname']; ?>" alt="">
